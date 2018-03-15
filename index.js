cded23eb042825859e3a20352e3364b6b613fc74
Steven Li <Steven.Li@walmartlabs.com>
2018-03-15 16:44:33 -0700

    functionality: demonstration of immutability of a blockchain

--- index.js
+++ index.js
@@ -10,5 +10,9 @@ jscoin.addBlock(new Block(2, "03/16/2018", {amount: 330}))
 // a blockchain will inherently be immutable once a new block has been added
 // aka you can't just change the information in a added block have have isChainValid == true anymore
 
-//initially = true
+//initially == true
+console.log("valid? =>", jscoin.isChainValid())
+
+//change the block even a little and the blockchain isn't valid anymore
+jscoin.chain[1].data.amount = 999999999999 //...making more cash...
 console.log("valid? =>", jscoin.isChainValid())
